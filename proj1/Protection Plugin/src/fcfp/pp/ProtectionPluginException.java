276129af0bbbd770a873e67eb17256a5de91d517
Simão Reis <dracostriker@hotmail.com>
2013-10-08 18:59:24 +0000

    Adding PPException Constructor with custom message error.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@7 a478ab85-490a-429d-afbb-08a0a9d09df7

--- proj1/Protection Plugin/src/fcfp/pp/ProtectionPluginException.java
+++ proj1/Protection Plugin/src/fcfp/pp/ProtectionPluginException.java
@@ -4,7 +4,20 @@ package fcfp.pp;
  * Protection Plugin Generic Exception.
  *
  * @author Simão Paulo Rato Alves Reis
- * @version 1.0
+ * @version 1.1
  */
 public class ProtectionPluginException extends Exception {
+
+    /**
+     * Default Exception Constructor.
+     */
+    public ProtectionPluginException() {
+    }
+
+    /**
+     * Default Exception Constructor with message error.
+     */
+    public ProtectionPluginException(String err) {
+        super(err);
+    }
 }
