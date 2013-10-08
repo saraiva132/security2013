cde280172fd2c13b7b6894c434b0594834721832
Simão Reis <dracostriker@hotmail.com>
2013-10-08 19:51:08 +0000

    Adding new test cases for xor cypher.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@11 a478ab85-490a-429d-afbb-08a0a9d09df7

---
+++ proj1/Xor Cypher/test/pp/XorTest.java
@@ -0,0 +1,86 @@
+package pp;
+
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.junit.Assert;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Simão Paulo Rato Alves Reis
+ * @version 1.0
+ */
+public class XorTest {
+
+    /**
+     * Test cypher method.
+     */
+    @Test
+    public void testCypher() {
+        Xor pp = new Xor();
+        byte[] msg = {0, 1, 2, 3, 4, 5};
+        byte[] key = {0, 1, 6, 2, 3, 4};
+        byte[] cpyMsg = new byte[msg.length];
+        System.arraycopy(msg, 0, cpyMsg, 0, cpyMsg.length);
+        pp.cypher(cpyMsg, key);
+        Assert.assertThat(msg, IsNot.not(IsEqual.equalTo(cpyMsg)));
+    }
+
+    /**
+     * Test decypher method.
+     */
+    @Test
+    public void testDecypher() {
+        Xor pp = new Xor();
+        byte[] msg = {0, 1, 2, 3, 4, 5};
+        byte[] key = {0, 1, 6, 2, 3, 4};
+        byte[] cpyMsg = new byte[msg.length];
+        System.arraycopy(msg, 0, cpyMsg, 0, cpyMsg.length);
+        pp.cypher(cpyMsg, key);
+        pp.decypher(cpyMsg, key);
+        assertArrayEquals(msg, cpyMsg);
+    }
+
+    /**
+     * Test cypher and decypher method with key.length smaller than msg.length.
+     */
+    @Test
+    public void testKeySmallerThanMsg() {
+        Xor pp = new Xor();
+        byte[] msg = {0, 1, 2, 3, 4, 5, 6, 7};
+        byte[] key = {0, 1, 6, 2, 3, 4};
+        byte[] cpyMsg = new byte[msg.length];
+        System.arraycopy(msg, 0, cpyMsg, 0, cpyMsg.length);
+        pp.cypher(cpyMsg, key);
+        pp.decypher(cpyMsg, key);
+        assertArrayEquals(msg, cpyMsg);
+    }
+
+    /**
+     * Test cypher and decypher method with msg.length smaller than key.length.
+     */
+    @Test
+    public void testMsgSmallerThanKey() {
+        Xor pp = new Xor();
+        byte[] msg = {0, 1, 2, 3, 4, 5, 6, 7};
+        byte[] key = {0, 1, 6, 2, 3, 4, 3, 2, 6, 4};
+        byte[] cpyMsg = new byte[msg.length];
+        System.arraycopy(msg, 0, cpyMsg, 0, cpyMsg.length);
+        pp.cypher(cpyMsg, key);
+        pp.decypher(cpyMsg, key);
+        assertArrayEquals(msg, cpyMsg);
+    }
+
+    /**
+     * Test cypher method with msg equal to key.
+     */
+    @Test
+    public void testMsgequalsKey() {
+        Xor pp = new Xor();
+        byte[] msg = {0, 1, 2, 3, 4, 5, 6, 7};
+        byte[] key = {0, 1, 2, 3, 4, 5, 6, 7};
+        pp.cypher(msg, key);
+        assertArrayEquals(msg, new byte[]{0, 0, 0, 0, 0, 0, 0, 0});
+    }
+}
