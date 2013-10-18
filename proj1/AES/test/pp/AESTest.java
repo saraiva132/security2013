5b122f6962ba13817401928f4a5fdacbf5b965b4
Simão Reis <dracostriker@hotmail.com>
2013-10-18 19:52:06 +0000

    Initial AES algorithm.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@28 a478ab85-490a-429d-afbb-08a0a9d09df7

---
+++ proj1/AES/test/pp/AESTest.java
@@ -0,0 +1,32 @@
+package pp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * AES Protection Plugin Test Cases
+ *
+ * @author Simão Paulo Rato Alves Reis
+ * @author Rafael Saraiva Figueiredo
+ * @version 1.0
+ */
+public class AESTest {
+
+    /**
+     * Test of cipher and decipher method, of class AES.
+     */
+    @Test
+    public void testAES_ValidKey() {
+        String text = "Ola como nhg nhg";
+        String pass = "3hd7s6h4453dgtredgdfgdfgdfg";
+        byte[] msg = text.getBytes();
+        System.out.println("length: " + msg.length + " " + "length: " + text.length() + " " + text);
+        byte[] key = pass.getBytes();
+        AES instance = new AES();
+        byte[] result = instance.cipher(msg, key);
+        System.out.println("length: " + result.length + " " + "length: " + new String(result).length() + " " + new String(result));
+        result = instance.decipher(result, key);
+        System.out.println("length: " + result.length + " " + "length: " + new String(result).length() + " " + new String(result));
+        assertArrayEquals(msg, result);
+    }
+}
