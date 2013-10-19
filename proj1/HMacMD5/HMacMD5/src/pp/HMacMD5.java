94cfa4363b84199d04352cbb6586bffb662afa24
Simão Reis <dracostriker@hotmail.com>
2013-10-19 15:29:55 +0000

    MD5 initial algorithm
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@33 a478ab85-490a-429d-afbb-08a0a9d09df7

---
+++ proj1/HMacMD5/HMacMD5/src/pp/HMacMD5.java
@@ -0,0 +1,35 @@
+package pp;
+
+import fcfp.pp.IntegrityPP;
+
+/**
+ *
+ * @author Simão Paulo Rato Alves Reis
+ * @author Rafael Saraiva Figueiredo
+ * @version 1.0
+ */
+public class HMacMD5 implements IntegrityPP {
+
+    private static final byte opad = 0x36;
+    private static final byte ipad = 0x5C;
+
+    @Override
+    public byte[] sign(byte[] msg, byte[] key) {
+        byte[] ikey = new byte[key.length];
+        for (int i = 0; i < ikey.length; i++) {
+            ikey[i] = (byte) (key[i] ^ ipad);
+        }
+        byte[] imsg = new byte[ikey.length + msg.length];
+        System.arraycopy(ikey, 0, imsg, 0, ikey.length);
+        System.arraycopy(msg, 0, imsg, ikey.length, msg.length);
+        byte[] hmsg = MD5.hash(new String(imsg)).getBytes();
+        byte[] okey = new byte[key.length];
+        for (int i = 0; i < okey.length; i++) {
+            okey[i] = (byte) (key[i] ^ ipad);
+        }
+        byte[] omsg = new byte[hmsg.length + okey.length];
+        System.arraycopy(okey, 0, omsg, 0, okey.length);
+        System.arraycopy(hmsg, 0, omsg, okey.length, hmsg.length);
+        return MD5.hash(new String(omsg)).getBytes();
+    }
+}
