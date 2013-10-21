2e64d9ffc58fed8e7fbe632ef519605f853e07f4
Simão Reis <dracostriker@hotmail.com>
2013-10-21 22:21:10 +0000

    Revert cast module.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@57 a478ab85-490a-429d-afbb-08a0a9d09df7

--- proj1/Flexible Cryptographic File Protection/src/fcfp/util/cast/ByteCast.java
+++ proj1/Flexible Cryptographic File Protection/src/fcfp/util/cast/ByteCast.java
@@ -17,19 +17,19 @@ public class ByteCast {
      */
     public static byte[] long2ByteArray(long num) {
         byte[] data = new byte[bytesPerLong];
-        long mask = 0x00000000000000FFL;
-        for (int i = 0; i < bytesPerLong; i++) {
-            data[i] = (byte) ((num & mask) >>> (i << 3));
-            mask = mask << Byte.SIZE;
-        }
-//        data[7] = (byte) ((num & 0xFF00000000000000L) >>> 56);
-//        data[6] = (byte) ((num & 0x00FF000000000000L) >>> 48);
-//        data[5] = (byte) ((num & 0x0000FF0000000000L) >>> 40);
-//        data[4] = (byte) ((num & 0x000000FF00000000L) >>> 32);
-//        data[3] = (byte) ((num & 0x00000000FF000000) >>> 24);
-//        data[2] = (byte) ((num & 0x0000000000FF0000) >>> 16);
-//        data[1] = (byte) ((num & 0x000000000000FF00) >>> 8);
-//        data[0] = (byte) ((num & 0x00000000000000FF));
+//        long mask = 0x00000000000000FFL;
+//        for (int i = 0; i < bytesPerLong; i++) {
+//            data[i] = (byte) ((num & mask) >>> (i << 3));
+//            mask = mask << Byte.SIZE;
+//        }
+        data[7] = (byte) ((num & 0xFF00000000000000L) >>> 56);
+        data[6] = (byte) ((num & 0x00FF000000000000L) >>> 48);
+        data[5] = (byte) ((num & 0x0000FF0000000000L) >>> 40);
+        data[4] = (byte) ((num & 0x000000FF00000000L) >>> 32);
+        data[3] = (byte) ((num & 0x00000000FF000000) >>> 24);
+        data[2] = (byte) ((num & 0x0000000000FF0000) >>> 16);
+        data[1] = (byte) ((num & 0x000000000000FF00) >>> 8);
+        data[0] = (byte) ((num & 0x00000000000000FF));
         return data;
     }
     
