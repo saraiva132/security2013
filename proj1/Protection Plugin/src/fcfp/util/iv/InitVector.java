7af1cf42e4ccd42603193c51f4bf01747b7842fb
Simão Reis <dracostriker@hotmail.com>
2013-10-22 19:26:25 +0000

    Adding IV feature to the PP API.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@71 a478ab85-490a-429d-afbb-08a0a9d09df7

---
+++ proj1/Protection Plugin/src/fcfp/util/iv/InitVector.java
@@ -0,0 +1,17 @@
+package fcfp.util.iv;
+
+/**
+ * Initialization Vector
+ *
+ * @author Simão Paulo Rato Alves Reis
+ * @version 1.0
+ */
+public final class InitVector {
+
+    /**
+     * This class cannot be instantiated or extended.
+     */
+    private InitVector() {
+    }
+    public static byte[] value; // the initial vector value
+}
