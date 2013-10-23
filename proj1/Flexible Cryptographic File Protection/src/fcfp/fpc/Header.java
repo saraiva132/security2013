c2cc1c93764dbe5f72fda463cd68e637b5e83db7
Rafael Figueiredo <rafael.saraiva@ua.pt>
2013-10-23 16:33:16 +0000

    header modificado
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@76 a478ab85-490a-429d-afbb-08a0a9d09df7

--- proj1/Flexible Cryptographic File Protection/src/fcfp/fpc/Header.java
+++ proj1/Flexible Cryptographic File Protection/src/fcfp/fpc/Header.java
@@ -14,58 +14,39 @@ import java.util.zip.CRC32;
 public class Header {
 
     private long padPos;
-    private long checksum;
-    private byte[] padding;
+    private static final byte[] TRUE = "TRUE".getBytes();
     private byte[] mac;
+
 
     public Header(byte[] header) {
         byte[] padPosStream = new byte[Byte.SIZE];
         System.arraycopy(header, 0, padPosStream, 0, 8);
         padPos = ByteCast.byteArray2Long(padPosStream);
-        byte[] checksumStream = new byte[Byte.SIZE];
-        System.arraycopy(header, 8, checksumStream, 0, 8);
-        checksum = ByteCast.byteArray2Long(checksumStream);
-        padding = new byte[256];
-        System.arraycopy(header, padPosStream.length + checksumStream.length, padding, 0, padding.length);
-        mac = new byte[header.length - padPosStream.length - checksumStream.length - padding.length];
-        System.arraycopy(header, padPosStream.length + checksumStream.length + padding.length, mac, 0, header.length - padPosStream.length - checksumStream.length - padding.length);
+        System.arraycopy(header, padPosStream.length, mac, 0, header.length - padPosStream.length);
+        mac = new byte[header.length - padPosStream.length];
+        System.arraycopy(header, padPosStream.length, mac, 0, header.length - padPosStream.length);
         System.out.println("padPos: " +padPos);
-        System.out.println("checksum: " + checksum);
     }
 
     public Header(long padPos, byte[] mac) {
-        this.checksum = 0;
         this.padPos = padPos;
         this.mac = mac;
-        this.padding = new byte[256];
-        Pad.fill(padding, 0, padding.length);
-        checksum = getChecksum();
     }
 
     public byte[] getStream() {
         byte[] padPosStream = ByteCast.long2ByteArray(padPos);
-        byte[] checksumStream = ByteCast.long2ByteArray(checksum);
-        byte[] header = new byte[padPosStream.length + checksumStream.length + mac.length + padding.length];
+        byte[] header = new byte[padPosStream.length + mac.length];
         System.arraycopy(padPosStream, 0, header, 0, padPosStream.length);
-        System.arraycopy(checksumStream, 0, header, padPosStream.length, checksumStream.length);
-        System.arraycopy(padding, 0, header, padPosStream.length + checksumStream.length, padding.length);
-        System.arraycopy(mac, 0, header, padPosStream.length + checksumStream.length + padding.length, mac.length);
+        System.arraycopy(mac, 0, header, padPosStream.length, mac.length);
         System.out.println("padPos: " + padPosStream[3] + padPosStream[2] + padPosStream[1] + padPosStream[0]);
-        System.out.println("checksum: " + checksumStream[3] + checksumStream[2] + checksumStream[1] + checksumStream[0]);
         return header;
     }
 
-    private long getChecksum() {
-        CRC32 crc = new CRC32();
-        crc.update(padding);
-        return crc.getValue();
-    }
 
     public boolean checksum() {
         long temp;
-        temp = getChecksum();
-        System.out.println(temp + " : " + checksum + " JustToCheck: PadPos - " + padPos + " why not mac: - " + mac.length);
-        return temp == checksum;
+        System.out.println( " : " + " JustToCheck: PadPos - " + padPos + " why not mac: - " + mac.length);
+        return true;
     }
 
     public byte[] getMac() {
