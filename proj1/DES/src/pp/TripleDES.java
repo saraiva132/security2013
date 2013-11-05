edb02e1d1ecb081fe37538a867327cf4abd64ecf
Rafael Figueiredo <rafael.saraiva@ua.pt>
2013-11-05 23:21:15 +0000

    triple des ligeiramente alterado
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@87 a478ab85-490a-429d-afbb-08a0a9d09df7

--- proj1/DES/src/pp/TripleDES.java
+++ proj1/DES/src/pp/TripleDES.java
@@ -39,13 +39,16 @@ public class TripleDES implements EncryptionPP {
         30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29,
         32};
     // key shift for each round
-    private static int[] keyShift = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2,
+    //Shift value altered; keyShiftOriginal = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1}
+    private static int[] keyShift = {1, 2, 3, 2, 1, 3, 3, 1, 1, 2, 2, 1, 2, 2,
         2, 1};
     // expansion permutation from function f
     private static int[] expandTbl = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
         9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21,
         20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,
         1};
+    //xor each round R1 = (Lo ^ xorRound) ^ f(Ro,Ki);
+    private static byte [] xorRound = {28,12,18,9};
     // substitution boxes
     private static int[][][] sboxes = {
         {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
@@ -159,7 +162,8 @@ public class TripleDES implements EncryptionPP {
             } else {
                 R = f_func(R, subkeys[i]);
             }
-            R = xor_func(L, R);
+        
+            R = xor_func(xor_func(L,xorRound), R);
             L = tmpR;
         }
 
