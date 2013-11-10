2ba0eee9820c858e0fcae0576ad382bf3beefa8b
Simão Reis <dracostriker@hotmail.com>
2013-11-10 02:39:55 +0000

    Cleaning code.
    git-svn-id: https://code.ua.pt/svn/security2013-p1g1@119 a478ab85-490a-429d-afbb-08a0a9d09df7

--- proj1/DES/src/pp/TripleDES.java
+++ proj1/DES/src/pp/TripleDES.java
@@ -14,43 +14,43 @@ import java.util.Arrays;
 public class TripleDES implements EncryptionPP {
 
     // initial permuation table
-    private static int[] IP = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36,
+    private static final int[] IP = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36,
         28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32,
         24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19,
         11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
     // inverse initial permutation
-    private static int[] invIP = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47,
+    private static final int[] invIP = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47,
         15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13,
         53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51,
         19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17,
         57, 25};
     // Permutation P (in f(Feistel) function)
-    private static int[] P = {16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5,
+    private static final int[] P = {16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5,
         18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4,
         25
     };
     // initial key permutation 64 => 56 biti
-    private static int[] PC1 = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34,
+    private static final int[] PC1 = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34,
         26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63,
         55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53,
         45, 37, 29, 21, 13, 5, 28, 20, 12, 4};
     // key permutation at round i 56 => 48
-    private static int[] PC2 = {14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+    private static final int[] PC2 = {14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
         23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55,
         30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29,
         32};
     // key shift for each round
     //Shift value altered; 
     //keyShift Original = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
-    private static int[] keyShift = {1, 2, 3, 2, 1, 3, 3, 1, 1, 2, 2, 1, 2, 2,2,1};
+    private static final int[] keyShift = {1, 2, 3, 2, 1, 3, 3, 1, 1, 2, 2, 1, 2, 2,2,1};
     
     // expansion permutation from function f
-    private static int[] expandTbl = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
+    private static final int[] expandTbl = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
         9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21,
         20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,
         1};
     // substitution boxes
-    private static int[][][] sboxes = {
+    private static final int[][][] sboxes = {
         {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
             {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
             {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
@@ -275,7 +275,7 @@ public class TripleDES implements EncryptionPP {
             key = new byte[24];
             System.arraycopy(keyz, 0, key,0, keyz.length);
         }
-        int lenght = 0;
+        int lenght;
         byte[] padding;
         int i;
 
